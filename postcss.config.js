6b28d1e1021551f6e2c92fc6035628d194d9b9c1
neolectron <neolectron@codinglab.io>
2021-02-18 23:58:15 +0100

    build(postcss): enable tailwindcss plugin

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
+/* eslint-env node */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
