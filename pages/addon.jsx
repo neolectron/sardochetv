084a73201048bf98392a8e6868bfa9335e5a5300
neolectron <neolectron@codinglab.io>
2021-02-19 02:40:54 +0100

    feat(page:addon): new addon/addons/index page using same layout.
    + assets

---
+++ pages/addon.jsx
@@ -0,0 +1,34 @@
+import Layout from '../component/Layout/Layout.jsx';
+import Card from '../component/Card/Card.jsx';
+import Icon from '../component/Icon/Icon.jsx';
+import Button from '../component/Button/Button.jsx';
+
+const AddonPage = () => (
+  <Layout className="items-center">
+    <img src="sardoche.png" alt="logotype sardoche" className="my-12" />
+    <div className="flex gap-8">
+      <Card className="max-w-5xl">
+        <div className="flex gap-8">
+          <Icon name="chrome" className="h-16 sm:h-24" />
+          <div className="flex-grow flex flex-col">
+            <div className="text-xl">Sardalert - Chrome</div>
+            <div className="flex-grow text-gray-600">(brave également)</div>
+            <Button text="Télécharger !" className="text-xs" />
+          </div>
+        </div>
+      </Card>
+
+      <Card className="max-w-5xl">
+        <div className="flex gap-8">
+          <Icon name="firefox" className="h-16 sm:h-24" />
+          <div className="flex-grow flex flex-col">
+            <div className="text-xl flex-grow">Sardalert - Firefox</div>
+            <Button text="Télécharger !" className="text-xs" />
+          </div>
+        </div>
+      </Card>
+    </div>
+  </Layout>
+);
+
+export default AddonPage;
