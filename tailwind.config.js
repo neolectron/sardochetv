80ead9db70b3714eecb939e663d7889077799c54
neolectron <neolectron@codinglab.io>
2021-02-19 17:00:21 +0100

    fix(build): tailwind
    fix tailwind strip css at build time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./component/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
