7462766411312f1c7ac3dd7ff0b0d1463fd7d21a
neolectron <neolectron@codinglab.io>
2021-02-23 22:26:31 +0100

    feat(adminPage): add admin page

---
+++ pages/admin/index.jsx
@@ -0,0 +1,29 @@
+import Layout from '../../component/Layout/Layout.jsx';
+import Button from '../../component/Button/Button.jsx';
+
+import { signIn, signOut, useSession } from 'next-auth/client';
+
+const adminPage = ({ adminList }) => {
+  const [session, loading] = useSession();
+
+  if (!session) {
+    return (
+      <Layout>
+        <Button icon="twitch" onClick={() => signIn('twitch')} text="Login !" />
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      You are logged-in.
+      <Button icon="twitch" onClick={() => signOut('twitch')} text="Logout !" />
+    </Layout>
+  );
+};
+
+export async function getStaticProps() {
+  return { props: { adminList: ['sardoche'] } };
+}
+
+export default adminPage;
