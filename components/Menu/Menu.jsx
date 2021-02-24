c650fe151fada6856c7caba2643ab299d8b8193f
neolectron <neolectron@codinglab.io>
2021-02-24 22:17:47 +0100

    feat(Menu): new side-menu

---
+++ components/Menu/Menu.jsx
@@ -0,0 +1,66 @@
+import { useRef, useState } from 'react';
+import Icon from '../Icon/Icon.jsx';
+import Button from '../Button/Button.jsx';
+
+import { useClickAway } from 'react-use';
+import { signOut } from 'next-auth/client';
+
+const Menu = ({ children = null, user = null }) => {
+  const menuRef = useRef(null);
+
+  const [open, setOpen] = useState(false);
+  const close = () => setOpen(false);
+
+  useClickAway(menuRef, close);
+  const openedClass = open ? 'block' : 'hidden';
+
+  return (
+    <nav
+      ref={menuRef}
+      className={`bg-white w-full flex flex-col justify-between md:w-80 md:h-full`}
+    >
+      <button
+        onClick={() => setOpen((x) => !x)}
+        className="md:hidden h-14 pl-2 focus:outline-none"
+      >
+        <Icon name="burger" height="32px" />
+      </button>
+      <div
+        className={`h-full flex flex-col justify-between font-semibold text-xl overflow-hidden text-gray-900 md:flex ${openedClass}`}
+      >
+        <div className="flex flex-col">
+          <div
+            className="h-28 bg-cover flex justify-center items-center"
+            style={{ backgroundImage: 'url(bg.png)' }}
+          >
+            <img src="tasse.png" className="h-12 mr-4" alt="logo brand" />
+            <p className="text-white font-bold text-2xl text-shadow select-none">
+              Sardoche.tv
+            </p>
+          </div>
+          {children}
+        </div>
+        {user && (
+          <div className="h-14 p-2 border-t border-gray-300 flex items-center">
+            <img
+              src={user.image}
+              alt="Image d'avatar"
+              className="h-full mr-4 rounded"
+            />
+            <p className="truncate" title={user.name}>
+              {user.name}
+            </p>
+            <Button
+              onClick={() => signOut('twitch')}
+              icon="logout"
+              text="Logout"
+              className="text-sm ml-auto"
+            />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#ff9e3b',
+      },
+    },
   },
   variants: {
     extend: {},
