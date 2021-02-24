17b48f8e96c911392cb9a12659118df8d2d05685
neolectron <neolectron@codinglab.io>
2021-02-24 22:18:42 +0100

    feat(Layout): new Layout with sideMenu and spread props

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,7 +1,35 @@
-const Layout = ({ children, className = '' }) => (
-  <>
-    <main className={`h-full flex flex-col ${className}`}>{children}</main>
-  </>
+import Menu from '../Menu/Menu.jsx';
+import Link from 'next/link';
+import Icon from '../Icon/Icon.jsx';
+
+const Layout = ({ menu = false, children, className = '', ...rest }) => (
+  <div className="w-full h-full flex flex-col md:flex-row">
+    {menu && (
+      <Menu {...rest}>
+        <Link href="/admin/addons">
+          <a className="h-14 p-4 pl-10 flex items-center transform transition-transform origin-left border-l-8 border-transparent  hover:border-orange hover:bg-yellow-100 hover:scale-110">
+            <Icon name="addon" height="1.5em" className="mr-5" />
+            Sardalert
+          </a>
+        </Link>
+        <Link href="/admin/sound">
+          <a className="h-14 p-4 pl-10 flex items-center transform transition-transform origin-left border-l-8 border-transparent hover:border-orange hover:bg-yellow-100 hover:scale-110">
+            <Icon name="soundwave" height="1.5em" className="mr-5" />
+            Sardsound
+          </a>
+        </Link>
+        <Link href="/admin/bots">
+          <a className="h-14 p-4 pl-10 flex items-center transform transition-transform origin-left border-l-8 border-transparent hover:border-orange hover:bg-yellow-100 hover:scale-110">
+            <Icon name="bot" height="1.5em" className="mr-5" />
+            Sardbot
+          </a>
+        </Link>
+      </Menu>
+    )}
+    <main className={`flex-grow h-full flex flex-col ${className}`}>
+      {children}
+    </main>
+  </div>
 );
 
 export default Layout;
