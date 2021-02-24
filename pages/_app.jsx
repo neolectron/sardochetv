2c417df83b3fe1368606dddc4d1667b84f36f26d
neolectron <neolectron@codinglab.io>
2021-02-24 23:15:15 +0100

    fix(Auth): fix next-auth context provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
-import "../styles/_app.css";
+import '../styles/_app.css';
+import { Provider } from 'next-auth/client';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
