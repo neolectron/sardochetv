5e387b9db62a1c220172c64e2633360047dec6f8
neolectron <neolectron@codinglab.io>
2021-02-24 22:14:38 +0100

    feat(Icon): added new Icons

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -2,3 +2,7 @@ export { default as chrome } from './icons/chrome.svg';
 export { default as firefox } from './icons/firefox.svg';
 export { default as twitch } from './icons/twitch.svg';
 export { default as burger } from './icons/burger.svg';
+export { default as logout } from './icons/logout.svg';
+export { default as bot } from './icons/bot.svg';
+export { default as soundwave } from './icons/soundwave.svg';
+export { default as addon } from './icons/addon.svg';
