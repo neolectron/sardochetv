637b0e5cdf12930f194e9d7343356dd916329531
neolectron <neolectron@codinglab.io>
2021-02-25 02:36:32 +0100

    feat(Card): title support + responsive-fix

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,5 +1,13 @@
-const Card = ({ children, text, className = '' }) => (
-  <div className={`p-4 bg-white rounded-md ${className}`}>
+const Card = ({
+  children = null,
+  text = null,
+  title = null,
+  className = '',
+}) => (
+  <div className={`p-4 bg-white rounded-md break-words w-max ${className}`}>
+    {title && (
+      <div className="pb-4 font-semibold text-lg text-center">{title}</div>
+    )}
     {children || text}
   </div>
 );
