271cfc903f2d6f10c49052d6b4d7b8b66ff58a6a
neolectron <neolectron@codinglab.io>
2021-02-28 02:45:46 +0100

    fix(auth): now Layout handle the authentication, + Spinner

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,29 +1,5 @@
 import Layout from '../../components/Layout/Layout.jsx';
-import Button from '../../components/Button/Button.jsx';
 
-import { signIn, useSession } from 'next-auth/client';
-
-const adminPage = ({ adminList }) => {
-  const [session, loading] = useSession();
-
-  if (!session) {
-    return (
-      <Layout className="items-center justify-center">
-        <Button
-          reversed
-          icon="twitch"
-          onClick={() => signIn('twitch')}
-          text="Login !"
-        />
-      </Layout>
-    );
-  }
-
-  return <Layout menu user={session.user}></Layout>;
-};
-
-export async function getStaticProps() {
-  return { props: { adminList: ['sardoche'] } };
-}
+const adminPage = () => <Layout checkauth menu></Layout>;
 
 export default adminPage;
