fe0af30b72f2a5d2eba50ed43e7a38340feafa22
neolectron <neolectron@codinglab.io>
2021-02-28 02:44:38 +0100

    fix(auth): now Layout handle the authentication, + Spinner

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,20 +1,55 @@
 import Menu from '../Menu/Menu.jsx';
+import Spinner from '../Spinner/Spinner.jsx';
+import { useSession, signIn } from 'next-auth/client';
 
-const Layout = ({ menu = false, children, className = '', ...rest }) => (
-  <div className="w-full h-full flex flex-col md:flex-row">
-    {menu && (
-      <Menu {...rest}>
-        <Menu.Item href="/admin/addons" icon="addon" text="Sardalert" />
-        <Menu.Item href="/admin/sound" icon="soundwave" text="Sardsound" />
-        <Menu.Item href="/admin/bots" icon="bot" text="Sardbot" />
-      </Menu>
-    )}
-    <main
-      className={`flex-1 flex-shrink-0 flex flex-col overflow-y-auto ${className}`}
-    >
-      {children}
-    </main>
-  </div>
-);
+const Layout = ({
+  menu = false,
+  checkauth = false,
+  children,
+  className = '',
+  ...rest
+}) => {
+  const [session, loading] = useSession();
+
+  if (checkauth && loading) {
+    return (
+      <Spinner
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
+  }
+
+  if (checkauth && !session?.user) {
+    return (
+      <Button
+        reversed
+        icon="twitch"
+        onClick={() => signIn('twitch')}
+        text="Login !"
+      />
+    );
+  }
+
+  return (
+    <div className="flex flex-col w-full h-full md:flex-row">
+      {menu && (
+        <Menu user={session?.user}>
+          <Menu.Item href="/admin/addons" icon="addon" text="Sardalert" />
+          <Menu.Item href="/admin/sound" icon="soundwave" text="Sardsound" />
+          <Menu.Item href="/admin/bots" icon="bot" text="Sardbot" />
+        </Menu>
+      )}
+      <main
+        className={`flex-1 flex-shrink-0 flex flex-col overflow-y-auto ${className}`}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
 
 export default Layout;
