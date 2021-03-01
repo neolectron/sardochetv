bf0e1122d3884e8b508c928c984d62dec6fb8c7e
neolectron <neolectron@codinglab.io>
2021-03-01 17:01:46 +0100

    style(eslint): enable eslint on workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,34 @@
 /* eslint-env node */
 module.exports = {
   env: {
-    "es2021": true,
+    es2021: true,
   },
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: { jsx: true },
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@next/next/recommended",
-    "plugin:jsx-a11y/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   settings: {
     react: {
-      version: "detect",
-    }
+      version: 'detect',
+    },
   },
   rules: {
-    "react/react-in-jsx-scope": "off", // React is no longer needed in scope
-    "react/prop-types": "off"
-  }
-}
+    'react/react-in-jsx-scope': 'off', // React is no longer needed in scope
+    'react/prop-types': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
