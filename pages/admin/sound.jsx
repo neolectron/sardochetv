b5a628ccbb12ca87f09c362cb3fee67dccf700d5
neolectron <xeolectron@gmail.com>
2021-04-01 02:48:38 +0200

    feat(SoundPage): remove grid layout, going full flex

--- pages/admin/sound.jsx
+++ pages/admin/sound.jsx
@@ -3,9 +3,11 @@ import { SoundUpload } from '../../components/Widgets/Widgets.js';
 
 const soundPage = () => {
   return (
-    <Layout checkauth menu>
-      <div className="grid grid-cols-1 gap-4 p-4 lg:grid-cols-2 2xl:grid-cols-3">
-        <SoundUpload />
+    <Layout checkauth menu className="p-8">
+      <div className="flex flex-wrap items-start flex-grow">
+        <div className="w-full p-4 h-72 lg:w-1/2 2xl:w-1/3">
+          <SoundUpload />
+        </div>
       </div>
     </Layout>
   );
