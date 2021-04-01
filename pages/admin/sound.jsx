05ba1227f5782c05412d35ec3a80ee82382ad142
neolectron <xeolectron@gmail.com>
2021-04-01 06:41:55 +0200

    fix(soundPage): remove mobile paddings + fix align-items

--- pages/admin/sound.jsx
+++ pages/admin/sound.jsx
@@ -3,8 +3,8 @@ import { SoundUpload, SoundList } from '../../components/Widgets/Widgets.js';
 
 const soundPage = () => {
   return (
-    <Layout checkauth menu className="p-8">
-      <div className="flex flex-wrap items-start flex-grow">
+    <Layout checkauth menu className="md:p-8">
+      <div className="flex flex-wrap content-start flex-grow">
         <div className="w-full p-4 h-72 lg:w-1/2 2xl:w-1/3">
           <SoundUpload />
         </div>
