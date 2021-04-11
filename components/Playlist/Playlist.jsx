7fae702bb59e36b16d7f66dc8f89b209fc5581f9
neolectron <xeolectron@gmail.com>
2021-04-11 18:24:19 +0200

    feat(Playlist): new playlist view with thumnails

--- components/Playlist/Playlist.jsx
+++ components/Playlist/Playlist.jsx
@@ -1,22 +1,19 @@
-import Button from '../Button/Button';
-
 const Playlist = ({ list = [], onItemChange }) => (
-  <div className="flex flex-col h-full p-4 bg-green-300">
-    <div className="flex">
-      <Button
-        icon="firefox"
-        text="GO PREV !"
-        onClick={() => onItemChange((val) => val - 1)}
-      />
-      <Button
-        icon="chrome"
-        text="GO NEXT !"
-        onClick={() => onItemChange((val) => val + 1)}
-      />
-    </div>
+  <div className="flex flex-col h-full">
     {list.map((video) => (
-      <div key={video.videoId} className="flex my-2 font-bold bg-white">
-        {video.title}
+      <div
+        key={video.videoId}
+        className="flex p-2  text-white transition-transform transform hover:scale-110"
+        onClick={() => onItemChange(video.videoId)}
+      >
+        <img
+          src={video.thumbnail_url}
+          height={video.thumbnail_height}
+          width={video.thumbnail_width}
+          className="object-cover w-20 h-20"
+          alt="video thumnail"
+        />
+        <div className="pl-4 text-sm">{video.title}</div>
       </div>
     ))}
   </div>
