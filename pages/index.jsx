99e19567aae9c6d8d5110a59bf31840c5fcaf505
neolectron <xeolectron@gmail.com>
2021-05-04 16:03:35 +0200

    feat(live.json): staticly build live.json in the public folder.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,25 @@
-export { default } from "./addon.jsx";
+export { default } from './addon.jsx';
+
+import { writeFile } from 'fs/promises';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { getLive } from '../queries/livealert.js';
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri:
+      'https://api-eu-central-1.graphcms.com/v2/cko0hcn1zwhn101xt5z2ehfwp/master',
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: getLive('sardoche'),
+  });
+
+  await writeFile('./public/live.json', JSON.stringify(data), {
+    encoding: 'utf-8',
+  });
+
+  return {
+    props: {},
+  };
+}
